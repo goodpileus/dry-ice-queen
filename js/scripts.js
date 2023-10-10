9f301960bb2b3a966c02df90750c0021d50323eb
Aidan Quinlan <aidankquinlan@gmail.com>
2023-10-09 19:08:13 -0700

    up to goosepimples

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,90 @@
+// init controller
+var controller = new ScrollMagic.Controller({globalSceneOptions: {
+	// duration: '100%'
+}});
 
-	// init controller
-	var controller = new ScrollMagic.Controller({globalSceneOptions: {
-    // duration: '100%'
-  }});
+// s1
+new ScrollMagic.Scene({triggerElement: "#drift"})
+				.setClassToggle(".drift-bg", "active")
+				.addIndicators()
+				.addTo(controller);
 
-	// build scenes
-	new ScrollMagic.Scene({triggerElement: "#sec1"})
-					.setClassToggle(".bg1", "active") // add class toggle
-					.setClassToggle(".bg2", "active") // add class toggle
-					// .setClassToggle("#bg", "inactive") // add class toggle
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
-	new ScrollMagic.Scene({triggerElement: "#sec2"})
-					.setClassToggle(".bg2", "active") // add class toggle
-					.setClassToggle(".bg3", "active") // add class toggle
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
-	new ScrollMagic.Scene({triggerElement: "#eyes"})
-					.setClassToggle(".bg3", "active") // add class toggle
-					.setClassToggle(".bg4", "active") // add class toggle
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
-	new ScrollMagic.Scene({triggerElement: "#sec3"})
-					.setClassToggle(".bg4", "active") // add class toggle
-					.setClassToggle(".bg5", "active") // add class toggle
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+new ScrollMagic.Scene({triggerElement: "#hd"})
+				.setClassToggle(".hd-bg", "active") 
+				.addIndicators() 
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#eyes"})
+				.setClassToggle(".eyes-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#loss"})
+				.setClassToggle(".loss-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#failure"})
+				.setClassToggle(".failure-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#goosepimples"})
+				.setClassToggle(".goosepimples-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#labyrinth"})
+				.setClassToggle(".labyrinth-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#voice"})
+				.setClassToggle(".voice-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#sunshine"})
+				.setClassToggle(".sunshine-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#night"})
+				.setClassToggle(".night-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#door"})
+				.setClassToggle(".door-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#corridor"})
+				.setClassToggle(".corridor-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#vulva"})
+				.setClassToggle(".vulva-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#awake"})
+				.setClassToggle(".awake-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#fade"})
+				.setClassToggle(".fade-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#blank"})
+				.setClassToggle(".blank-bg", "active")
+				.addIndicators()
+				.addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#end"})
+				.setClassToggle(".end-bg", "active")
+				.addIndicators()
+				.addTo(controller);
